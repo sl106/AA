9f98dc3e27a549b8c0b17811e0d26fdbcedf64e1
aistegru <agrusnyte@gmail.com>
2019-04-18 14:12:38 +0100

    added a bracket

--- lambda/py/alexa/data.py
+++ lambda/py/alexa/data.py
@@ -7,7 +7,7 @@ START_QUIZ_MESSAGE = ("I've been told to tell you to drink responsibly. Let's be
 SINGLE_PLAYER_MESSAGE = ("Are you sure you want to play by yourself? "
                         "<prosody pitch=\"x-high\">Alexa, this is so sad, play despacito.</prosody> <break time=\"2s\"/>"
                         "<voice name=\"Celine\"><lang xml:lang=\"fr-FR\">Joking!</lang></voice> "
-                        "<emphasis level="strong">Last chance though,</emphasis> "
+                        "<emphasis level="strong">Last chance though,</emphasis> ")
 
 NEXT_ROUND_MESSAGE = "Okay! Time for round "
 
