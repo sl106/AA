5b0d37db25fb84de7658022060c752228220e8cc
isylynch <isylynch@hotmail.com>
2019-04-18 15:59:00 +0100

    working for capitals but issue for hp

--- lambda/py/lambda_function.py
+++ lambda/py/lambda_function.py
@@ -332,14 +332,15 @@ class QuizAnswerHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
         attr = handler_input.attributes_manager.session_attributes
-        return (not is_intent_name("NewThemeIntent")(handler_input) and
+        return ((is_intent_name("AnswerIntent")(handler_input) or 
+                (not is_intent_name("NewThemeIntent")(handler_input) and
                 not is_intent_name("AMAZON.CancelIntent")(handler_input) and
                 not is_intent_name("AMAZON.StopIntent")(handler_input) and
                 not is_intent_name("AMAZON.PauseIntent")(handler_input) and
                 not is_intent_name("AMAZON.HelpIntent")(handler_input) and
                 not is_intent_name("UnsureIntent")(handler_input) and
                 not is_intent_name("AMAZON.FallbackIntent")(handler_input) and
-                not is_intent_name("AMAZON.RepeatIntent") and
+                not is_intent_name("AMAZON.RepeatIntent"))) and
                 attr.get("status") == "question")
 
     def handle(self, handler_input):
