c1e30abb57f3150b5716a429c086f9aaeaebaa9d
aistegru <agrusnyte@gmail.com>
2019-04-18 14:21:01 +0100

    removed celine

--- lambda/py/alexa/data.py
+++ lambda/py/alexa/data.py
@@ -5,8 +5,8 @@ WELCOME_MESSAGE = ("Welcome to that drinking game! ")
 START_QUIZ_MESSAGE = ("I've been told to tell you to drink responsibly. Let's begin! Which theme would you like for round ")
 
 SINGLE_PLAYER_MESSAGE = ("Are you sure you want to play by yourself? "
-                        "<prosody pitch=\"x-high\">Alexa, this is so sad, play despacito.</prosody> <break time=\"2s\"/>"
-                        "<voice name=\"Celine\"><lang xml:lang=\"fr-FR\">Joking!</lang></voice> "
+                        "<prosody pitch=\"x-high\">Alexa, this is so sad, play despacito.</prosody> <break time=\"0.5sec\"/>"
+                        "<lang xml:lang=\"fr-FR\">Joking!</lang> "
                         "<emphasis level=\"strong\">Last chance though,</emphasis> ")
 
 NEXT_ROUND_MESSAGE = "Okay! Time for round "
